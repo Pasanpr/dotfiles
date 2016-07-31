d5b11470d3ab8785a8d7a33b52472b2112504c18
Pasan Premaratne <pasan@premaratne.me>
2016-07-31 11:24:12 -0400

    Updated homebrew install script path

---
+++ Scripts/install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Run all dotfiles installers.
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+echo "Changing shell..."
+chsh -s /bin/zsh
+
+source ./homebrew/install.sh

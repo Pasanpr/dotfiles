2a4f8e227534598e69dca017302b89e8baf3a976
Pasan Premaratne <pasan@premaratne.me>
2016-08-05 10:40:10 -0400

    Create Customizations dir if missing

--- Scripts/themes.sh
+++ Scripts/themes.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-if [ -d "~/Customizations/themes/tomorrow-theme/" ]
+if ! [ -d "~/Customizations/themes/"]
 then
+  mkdir -p ~/Customizations/themes/
+
   cd ~/Customizations/themes/
   echo "Cloning tomorrow-theme"
 
   git clone https://github.com/Pasanpr/tomorrow-theme.git
 
-  cd ~/dotfiles
+  cd ~/.dotfiles
 fi
 
 source ./Xcode/install.sh
--- Xcode/install.sh
+++ Xcode/install.sh
@@ -5,5 +5,6 @@
 # This installs preferred Xcode themes
 
 echo "Copying Xcode theme"
+
 mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 cp ~/Customizations/themes/tomorrow-theme/Xcode\ 4/*.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/

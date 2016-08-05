bb6317b19eacf1098e3a103caead9e7d0d956e2c
Pasan Premaratne <pasan@premaratne.me>
2016-08-05 12:24:21 -0400

    Add .zshrc symlink. Included atom package installer

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -2,31 +2,17 @@
 #
 # Run all dotfiles installers.
 
-info () {
-  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
-}
-
-user () {
-  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
-}
-
-success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
-}
-
-fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-  echo ''
-  exit
-}
-
 echo "Changing shell..."
 chsh -s /bin/zsh
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 echo "Installing Homebrew..."
 source ./homebrew/install.sh
 
 echo "Installing themes"
 source ./Scripts/themes.sh
+
+echo "Installing atom packages"
+source ./atom/install.sh
 
 exit 0

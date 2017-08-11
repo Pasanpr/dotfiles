de707fb5fc01b17a6fa4ae25ebb21dbc32c0c491
Pasan Premaratne <pasan@premaratne.me>
2017-08-11 17:55:40 -0400

    Added git smart log

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,3 +6,4 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gac='git add -A && git commit -m'
 alias ga='git add'
+alias gsl="git log --graph --pretty=format:'commit: %C(bold red)%h%Creset %C(red)<%H>%Creset %C(bold magenta)%d %Creset%ndate: %C(bold yellow)%cd %Creset%C(yellow)%cr%Creset%nauthor: %C(bold blue)%an%Creset %C(blue)<%ae>%Creset%n%C(cyan)%s%n%Creset'"

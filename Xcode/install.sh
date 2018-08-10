3d4949abe5f444e11671cb26b08f5d5c88987b1c
Pasan Premaratne <pasan@premaratne.me>
2018-08-10 16:58:42 -0400

    Added xcversion tool to automate xcode installations

--- Xcode/install.sh
+++ Xcode/install.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 #
 # Xcode
-#
-# This installs preferred Xcode themes
+
+gem install xcode-install
+xcversion update
+
+function install_xode () {
+	echo "Which version of Xcode do you want to install:"
+	read version
+
+	xcversion install $version 
+}
+
+echo "Listing versions of Xcode.."
+xcversion list
+install_xode
+
+echo "Do you want to install another version? [y/n]:"
+read answer
+
+while [ "$answer" = "y" ]; do
+	install_xode
+	echo "Do you want to install another version? [y/n]:"
+	read answer
+done
+
+echo "Installing Xcode themes.."
 
 xcode_themes_dir=~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 

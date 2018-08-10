3591c10127d4ba99ec05e9b5333f555a29e19ae9
Pasan Premaratne <pasan@premaratne.me>
2018-08-10 14:44:39 -0400

    Add brew doctor

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -10,6 +10,7 @@ command -v brew >/dev/null 2>&1 || { echo >&2 "Homebrew unavailable. Instaling..
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
 
 brew update
+brew doctor
 brew tap caskroom/cask
 brew install brew-cask
 brew cleanup

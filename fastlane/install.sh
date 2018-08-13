5aa4c67e27736baed808fce8bd3d6955c80c4760
Pasan Premaratne <pasan@premaratne.me>
2018-08-13 12:40:15 -0400

    Check if fastlane is installed

--- fastlane/install.sh
+++ fastlane/install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+FAST=$(gem list -i fastlane)
+if ! [ -x "$RBENV" ]; then
+	gem install fastlane
+
 # Setup credentials
 fastlane fastlane-credentials add --username pasan@premaratne.me
+else
+	echo "fastlane already installed..."
+fi

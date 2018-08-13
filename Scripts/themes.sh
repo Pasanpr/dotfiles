add35449b6eadb5941f8a762320b10fc9e1488f9
Pasan Premaratne <pasan@premaratne.me>
2018-08-13 12:40:48 -0400

    Output message if no changes are made

--- Scripts/themes.sh
+++ Scripts/themes.sh
@@ -10,21 +10,26 @@ fi
 
 cd $themes_dir
 
-if ! test -d ~/Customizations/themes/tomorrow-theme;
-	then
+if ! test -d ~/Customizations/themes/tomorrow-theme; then
 	echo "Installing tomorrow-theme"
 	git clone https://github.com/Pasanpr/tomorrow-theme.git
+else
+	echo "Tomorrow theme installed"
 fi
 
-if ! test -d ~/Customizations/themes/afterglow-itermcolors;
-	then
+if ! test -d ~/Customizations/themes/afterglow-itermcolors; then
 	echo "Installing Afterglow theme for iTerm"
 	git clone https://github.com/yabatadesign/afterglow-itermcolors/
+else
+	echo "Afterglow theme installed"
 fi
 
-if ! test -d ~/Customizations/themes/fonts;
-	then
+if ! test -d ~/Customizations/themes/fonts; then
 	echo "Installing powerline fonts"
 	git clone https://github.com/powerline/fonts.git
 	source ./fonts/install.sh
+else
+	echo "powerline fonts installed"
 fi
+
+cd ~/.dotfiles

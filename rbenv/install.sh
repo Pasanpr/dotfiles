221dae172a66810ef9a9bcc89918808961f1f3fd
Pasan Premaratne <pasan@premaratne.me>
2018-08-13 12:39:52 -0400

    Check if rbenv is installed first. Set global ruby and rehash

--- rbenv/install.sh
+++ rbenv/install.sh
@@ -2,8 +2,16 @@
 #
 # rbenv
 
-brew install 'rbenv'
-rbenv init
 
-# Check if setup correctly
+RBENV=$(command -v rbenv)
+if ! [ -x "$RBENV" ]; then
+	brew install 'rbenv'
+	rbenv init
+	rbenv install 2.5.1
+	rbenv global 2.5.1
+	rbenv rehash
+	# Check if setup correctly
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
+else
+	echo "$(rbenv --version) installed"
+fi
